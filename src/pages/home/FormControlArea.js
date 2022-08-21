5a41041673a219fe9e6cfcef72791e7ca7fb661a
mohammadshariful <mohammadsharifulislam64@gmail.com>
2022-08-21 10:58:24 +0600

    form validation completed

--- src/pages/home/FormControlArea.js
+++ src/pages/home/FormControlArea.js
@@ -1,51 +1,81 @@
+import { useFormik } from "formik";
 import React from "react";
-
+import useFormControl from "../../hooks/useFormControl";
 const FormControlArea = () => {
+  const { initialValues, onSubmit, validate } = useFormControl();
+  const formik = useFormik({
+    initialValues: initialValues,
+    onSubmit: onSubmit,
+    validate: validate,
+  });
+
   return (
     <section className="bg-white rounded-lg shadow-lg p-2 md:p-3 lg:p-5">
-      <form>
+      <form onSubmit={formik.handleSubmit}>
         <div>
           <h2 className="font-semibold text-lg md:text-xl">Personal Details</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 personal details">
             <div className="form-control">
               <label className="label">
                 <span className="label-text text text-base font-normal">
-                  Name
+                  Name <small className="text-red-500"> &#9733;</small>
                 </span>
               </label>
 
               <input
+                onChange={formik.handleChange}
+                value={formik.values.name}
+                name="name"
                 type="text"
                 placeholder="Enter Name"
                 className="input input-bordered"
               />
+              {formik.errors.name && (
+                <p className="text-error font-semibold">{formik.errors.name}</p>
+              )}
             </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text text text-base font-normal">
                   Date of Birth or Age
+                  <small className="text-red-500"> &#9733;</small>
                 </span>
               </label>
 
               <input
+                onChange={formik.handleChange}
+                value={formik.values.dateOfBirth}
+                name="dateOfBirth"
                 type="text"
                 placeholder="DD/MM/YYY or Age in Years"
                 className="input input-bordered"
               />
+              {formik.errors.dateOfBirth && (
+                <p className="text-error font-semibold">
+                  {formik.errors.dateOfBirth}
+                </p>
+              )}
             </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text text text-base font-normal">
                   Sex
+                  <small className="text-red-500"> &#9733;</small>
                 </span>
               </label>
-              <select className="select select-bordered w-full ">
-                <option disabled selected>
-                  Enter Sex
-                </option>
-                <option>Han Solo</option>
-                <option>Greedo</option>
+              <select
+                onChange={formik.handleChange}
+                value={formik.values.sex}
+                name="sex"
+                className="select select-bordered w-full "
+              >
+                <option selected>Enter Sex</option>
+                <option>Male</option>
+                <option>Female</option>
               </select>
+              {formik.errors.sex && (
+                <p className="text-error font-semibold">{formik.errors.sex}</p>
+              )}
             </div>
             <div className="form-control">
               <label className="label">
@@ -55,6 +85,9 @@ const FormControlArea = () => {
               </label>
 
               <input
+                onChange={formik.handleChange}
+                value={formik.values.mobile}
+                name="mobile"
                 type="number"
                 placeholder="Enter Mobile"
                 className="input input-bordered"
@@ -67,14 +100,22 @@ const FormControlArea = () => {
                 </span>
               </label>
               <div className="flex  flex-col lg:flex-row lg:space-x-4 space-y-4 lg:space-y-0">
-                <select className="select select-bordered w-full max-w-xs">
-                  <option disabled selected>
-                    ID Type
-                  </option>
-                  <option>Han Solo</option>
-                  <option>Greedo</option>
+                <select
+                  onChange={formik.handleChange}
+                  value={formik.values.govtIdIssued}
+                  name="govtIdIssued"
+                  className="select select-bordered w-full max-w-xs"
+                >
+                  <option selected>ID Type</option>
+                  <option>U.S. Passport</option>
+                  <option>U.S. Passport Card</option>
+                  <option>Tribal Card</option>
+                  <option>Foreign Passport</option>
                 </select>
                 <input
+                  onChange={formik.handleChange}
+                  value={formik.values.govtId}
+                  name="govtId"
                   type="text"
                   placeholder="Enter Govt ID"
                   className="input input-bordered  max-w-xs"
@@ -89,34 +130,37 @@ const FormControlArea = () => {
             <div className="form-control">
               <label className="label">
                 <span className="label-text text text-base font-normal">
-                  Govt Issued ID
+                  Guardian Details
                 </span>
               </label>
               <div className="flex  flex-col lg:flex-row lg:space-x-4 space-y-4 lg:space-y-0">
-                <select className="select select-bordered w-full ">
-                  <option disabled selected>
-                    ID Type
-                  </option>
-                  <option>Han Solo</option>
-                  <option>Greedo</option>
+                <select
+                  onChange={formik.handleChange}
+                  value={formik.values.gurdianLevel}
+                  name="gurdianLevel"
+                  className="select select-bordered w-full "
+                >
+                  <option selected>Enter Level</option>
+                  <option>Father</option>
+                  <option>Mother</option>
+                  <option>Sister</option>
+                  <option>Brother</option>
                 </select>
-                <input
-                  type="text"
-                  placeholder="Enter Govt ID"
-                  className="input input-bordered  max-w-xs"
-                />
               </div>
             </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text text text-base font-normal">
-                  Email
+                  Enter Gurdain Name
                 </span>
               </label>
 
               <input
-                type="email"
-                placeholder="Enter Email"
+                onChange={formik.handleChange}
+                value={formik.values.gurdianName}
+                name="gurdianName"
+                type="text"
+                placeholder=" Enter Gurdain Name"
                 className="input input-bordered"
               />
             </div>
@@ -128,6 +172,9 @@ const FormControlArea = () => {
               </label>
 
               <input
+                onChange={formik.handleChange}
+                value={formik.values.emergencyNumber}
+                name="emergencyNumber"
                 type="text"
                 placeholder="Emergency Contact Number"
                 className="input input-bordered"
@@ -146,6 +193,9 @@ const FormControlArea = () => {
               </label>
 
               <input
+                onChange={formik.handleChange}
+                value={formik.values.address}
+                name="address"
                 type="text"
                 placeholder="Enter Address"
                 className="input input-bordered"
@@ -159,6 +209,9 @@ const FormControlArea = () => {
               </label>
 
               <input
+                onChange={formik.handleChange}
+                value={formik.values.state}
+                name="state"
                 type="text"
                 placeholder="Enter State"
                 className="input input-bordered"
@@ -172,6 +225,9 @@ const FormControlArea = () => {
               </label>
 
               <input
+                onChange={formik.handleChange}
+                value={formik.values.city}
+                name="city"
                 type="text"
                 placeholder="Enter city/town/village"
                 className="input input-bordered"
@@ -185,6 +241,9 @@ const FormControlArea = () => {
               </label>
 
               <input
+                onChange={formik.handleChange}
+                value={formik.values.country}
+                name="country"
                 type="text"
                 placeholder="Enter Country"
                 className="input input-bordered"
@@ -198,6 +257,9 @@ const FormControlArea = () => {
               </label>
 
               <input
+                onChange={formik.handleChange}
+                value={formik.values.pincode}
+                name="pincode"
                 type="text"
                 placeholder="Enter Pincode"
                 className="input input-bordered"
@@ -216,6 +278,9 @@ const FormControlArea = () => {
               </label>
 
               <input
+                onChange={formik.handleChange}
+                value={formik.values.occupation}
+                name="occupation"
                 type="text"
                 placeholder="Enter occupation"
                 className="input input-bordered"
@@ -227,12 +292,17 @@ const FormControlArea = () => {
                   Religion
                 </span>
               </label>
-              <select className="select select-bordered w-full ">
-                <option disabled selected>
-                  Enter Religion
-                </option>
-                <option>Han Solo</option>
-                <option>Greedo</option>
+              <select
+                onChange={formik.handleChange}
+                value={formik.values.religion}
+                name="religion"
+                className="select select-bordered w-full "
+              >
+                <option selected>Enter Religion</option>
+                <option>Islam</option>
+                <option>Hinduism</option>
+                <option>Buddhism</option>
+                <option>Christianity</option>
               </select>
             </div>
             <div className="form-control">
@@ -241,12 +311,15 @@ const FormControlArea = () => {
                   Marital Status
                 </span>
               </label>
-              <select className="select select-bordered w-full ">
-                <option disabled selected>
-                  Enter Marital Status
-                </option>
-                <option>Han Solo</option>
-                <option>Greedo</option>
+              <select
+                onChange={formik.handleChange}
+                value={formik.values.mariatal}
+                name="mariatal"
+                className="select select-bordered w-full "
+              >
+                <option selected>Enter Marital Status</option>
+                <option>Unmarried</option>
+                <option>Married</option>
               </select>
             </div>
             <div className="form-control">
@@ -255,12 +328,21 @@ const FormControlArea = () => {
                   Blood Group
                 </span>
               </label>
-              <select className="select select-bordered w-full ">
-                <option disabled selected>
-                  Group
-                </option>
-                <option>Han Solo</option>
-                <option>Greedo</option>
+              <select
+                onChange={formik.handleChange}
+                value={formik.values.blood}
+                name="blood"
+                className="select select-bordered w-full "
+              >
+                <option selected>Group</option>
+                <option>A+</option>
+                <option>A-</option>
+                <option> B+</option>
+                <option> B-</option>
+                <option> O+</option>
+                <option> O-</option>
+                <option>AB+ </option>
+                <option>AB- </option>
               </select>
             </div>
             <div className="form-control">
@@ -271,6 +353,9 @@ const FormControlArea = () => {
               </label>
 
               <input
+                onChange={formik.handleChange}
+                value={formik.values.nationality}
+                name="nationality"
                 type="text"
                 placeholder="Enter Nationality"
                 className="input input-bordered"
@@ -279,7 +364,10 @@ const FormControlArea = () => {
           </div>
         </div>
         <div className="flex justify-end space-x-4 mt-4">
-          <button type="reset" className="btn btn-md btn-error">
+          <button
+            type="reset"
+            className="btn btn-md bg-transparent border border-red-600 text-red-500"
+          >
             CANCEL
           </button>
           <button type="submit" className="btn btn-md btn-secondary text-white">
